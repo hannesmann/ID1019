c8aecca80d5452ee88cf032e9c086a7749bfc2ca
Hannes Mann <hannesmann2000@gmail.com>
2023-02-03 17:51:25 +0100

    math: Update test

--- src/math/main.exs
+++ src/math/main.exs
@@ -3,6 +3,8 @@ Code.require_file("evaluator.ex")
 
 defmodule Test do
   def eval_and_print(expr, vars) do
+    IO.inspect(expr)
+    IO.inspect(vars)
     result = MathEvaluator.eval_and_reduce(expr, vars)
 
     case result do
@@ -14,3 +16,4 @@ end
 
 Test.eval_and_print({:div, {:mul, {:num, 5}, {:num, 2}}, {:num, 5645}}, Environment.new())
 Test.eval_and_print({:mul, {:num, 5}, {:var, :x}}, Environment.new(%{:x => {:num, 10}}))
+Test.eval_and_print({:add, {:add, {:mul, {:num, 2}, {:var, :x}}, {:num, 3}}, {:q, 1,2}}, Environment.new(%{:x => {:num, 80}}))
